20a665e510a51ce275c3f423ac191a23ecbc2166
BunnyHog793 <czueh332@students.bju.edu>
2025-01-23 16:07:11 -0500

    working on stuff

--- scripts/node_2d.gd
+++ scripts/node_2d.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
+@export var base_speed : float = 500
 @export var move_speed : float = 500
-@export var roll_speed : float = 750
-@export var roll_time : float = .2
-@export var roll_timeout : float = .4
+@export var run_speed : float = 750
+@export var roll_speed : float = 1000
+@export var roll_time : float = .15
+@export var roll_timeout : float = .2
 
 var rolling : bool = false
 var rolling_speed = 0
@@ -18,28 +20,35 @@ enum state {
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	rolling = false
-	
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func get_input_direction():
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
-	if Input.get_action_strength("roll") && not rolling:
+	return input_direction
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var input_direction = get_input_direction()
+	
+	if Input.is_action_just_pressed("roll") && not rolling:
 		rolling = true
 		$roll_timer.start(roll_time)
 		$roll_timeout.start(roll_timeout)
 		rolling_speed = roll_speed
 		
 	
+	if Input.get_action_strength("run"):
+		move_speed = run_speed
+	else:
+		move_speed = base_speed
+	
+	
 	input_direction.normalized()
 	
 	velocity = input_direction * (move_speed + rolling_speed) 
 	
-	
-		
 	move_and_slide()
 
 
